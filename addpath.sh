0ed6c0242a21c405b37a8274d5044752169e23b8
leohe666 <leohe666@gmail.com>
2021-12-02 10:59:01 +0800

    add air hotreload

--- addpath.sh
+++ addpath.sh
@@ -4,3 +4,4 @@
 project_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # 将脚本目录添加到用户shell配置文件中
 echo 'PATH='$project_path':$PATH' >> ~/.bashrc
+curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $PWD/packages/bin
